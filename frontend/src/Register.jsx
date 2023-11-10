14e2a37a1d41d520be05ddbaea905a9c5c3be0e1
Ina Yoon <yia0319@gmail.com>
2023-11-09 21:42:44 -0500

    Login page and Register page

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Register() {
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign-up</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              name="name"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              name="password"
+              className="form-control rounded-0"
+            />
+          </div>
+          <button type="submit" className="btn btn-success w-100 rounded-0">
+            Sign up
+          </button>
+          <p>You agree to our terms and policy</p>
+          <Link
+            to="/login"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Login
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+}
