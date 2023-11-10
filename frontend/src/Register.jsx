9950055f51be8cf2d0e7efd47f586a9b59ea0759
Ina Yoon <yia0319@gmail.com>
2023-11-09 22:18:22 -0500

    Connect db with register page

--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,12 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function Register() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8081/register", values)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate("/login");
+        } else {
+          alert("Error");
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Sign-up</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label htmlFor="name">
               <strong>Name</strong>
@@ -16,17 +44,19 @@ export default function Register() {
               placeholder="Enter Your Name"
               name="name"
               className="form-control rounded-0"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
             <label htmlFor="email">
-              <strong>Name</strong>
+              <strong>Email</strong>
             </label>
             <input
               type="email"
               placeholder="Enter Your Email"
               name="email"
               className="form-control rounded-0"
+              onChange={handleChange}
             />
           </div>
           <div className="mb-3">
@@ -38,6 +68,7 @@ export default function Register() {
               placeholder="Enter Your Password"
               name="password"
               className="form-control rounded-0"
+              onChange={handleChange}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
+const salt = 10;
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,18 @@ const db = mysql2.createConnection({
   user: "root",
   password: "1234",
   database: "signup",
+});
+
+app.post("/register", (req, res) => {
+  const sql = "INSERT INTO login (`name`,`email`,`password`) VALUES (?)";
+  bcrypt.hash(req.body.password.toString(), salt, (err, hash) => {
+    if (err) return res.json({ Error: "Error for hashing password" });
+    const values = [req.body.name, req.body.email, hash];
+    db.query(sql, [values], (err, result) => {
+      if (err) return res.json({ Error: "Inserting data failed" });
+      return res.json({ Status: "Success" });
+    });
+  });
 });
 
 app.listen(8081, () => {
